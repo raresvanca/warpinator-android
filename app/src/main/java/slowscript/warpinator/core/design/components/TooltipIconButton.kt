ac64c6c56d460778046a20f5ebc6b56c41489cc7
Rares Vanca <raresvanca@gmail.com>
2026-01-24 17:16:13 +0200

    Tooltip Icon Button

---
+++ app/src/main/java/slowscript/warpinator/core/design/components/TooltipIconButton.kt
@@ -0,0 +1,39 @@
+package slowscript.warpinator.core.design.components
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.LocalContentColor
+import androidx.compose.material3.PlainTooltip
+import androidx.compose.material3.Text
+import androidx.compose.material3.TooltipAnchorPosition
+import androidx.compose.material3.TooltipBox
+import androidx.compose.material3.TooltipDefaults
+import androidx.compose.material3.rememberTooltipState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TooltipIconButton(
+    description: String,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    onClick: () -> Unit,
+    icon: ImageVector,
+    tint: Color = LocalContentColor.current,
+) {
+    TooltipBox(
+        positionProvider = TooltipDefaults.rememberTooltipPositionProvider(TooltipAnchorPosition.Below),
+        tooltip = { PlainTooltip { Text(description) } },
+        state = rememberTooltipState(),
+    ) {
+        IconButton(onClick = onClick, enabled = enabled, modifier = modifier) {
+            Icon(
+                icon, contentDescription = description, tint = tint,
+            )
+        }
+    }
+}
